4983d5ab78a16946c3c85af67bfa18263cf3e8e1
anjanipodomi <anjanidihapsari@gmail.com>
2025-10-24 10:38:53 +0700

    menambahkan spacer untuk space dengan jarak 30 dp

--- app/src/main/java/com/example/myapplication/UIPertama.kt
+++ app/src/main/java/com/example/myapplication/UIPertama.kt
@@ -50,6 +50,8 @@ fun ActivitasPertama(modifier: Modifier) {
                         .size(size= 100.dp)
                         .padding(all= 5.dp)
                 )
+                Spacer(modifier = Modifier.width(width= 30.dp))
+
 
 
 
