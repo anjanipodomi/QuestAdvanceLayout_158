84eb5019e8108aaccadba1322d1798466664d10a
anjanipodomi <anjanidihapsari@gmail.com>
2025-10-24 10:19:04 +0700

    Menambahkan elemen teks dan spacer pada composable ActivitasPertama

--- app/src/main/java/com/example/myapplication/UIPertama.kt
+++ app/src/main/java/com/example/myapplication/UIPertama.kt
@@ -22,4 +22,14 @@ fun ActivitasPertama(modifier: Modifier) {
             .padding(top = 100.dp)
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
-    )
+    ) {
+        Text(
+            text = stringResource(id= R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text=stringResource(id= R.string.univ),
+            fontSize = 22.sp
+        )
+        Spacer(modifier = Modifier.height(height = 25.dp))
