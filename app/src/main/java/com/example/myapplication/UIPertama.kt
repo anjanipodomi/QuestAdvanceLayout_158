7107fb123d2a4da4ec8b4bbea59075bf1b8dc7c0
anjanipodomi <anjanidihapsari@gmail.com>
2025-10-24 10:20:43 +0700

    menambahkan card

--- app/src/main/java/com/example/myapplication/UIPertama.kt
+++ app/src/main/java/com/example/myapplication/UIPertama.kt
@@ -33,3 +33,12 @@ fun ActivitasPertama(modifier: Modifier) {
             fontSize = 22.sp
         )
         Spacer(modifier = Modifier.height(height = 25.dp))
+        Card(
+            modifier = Modifier
+                .fillMaxWidth(fraction= 1f)
+                .padding(all= 12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.Black
+            )
+
+
