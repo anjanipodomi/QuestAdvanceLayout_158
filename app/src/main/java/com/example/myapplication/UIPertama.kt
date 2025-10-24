00e1fb7a2729ed3b4344ca20c58a12a667e097d6
anjanipodomi <anjanidihapsari@gmail.com>
2025-10-24 10:47:46 +0700

    Menambahkan Modifier.padding(bottom = 50.dp)

--- app/src/main/java/com/example/myapplication/UIPertama.kt
+++ app/src/main/java/com/example/myapplication/UIPertama.kt
@@ -24,43 +24,43 @@ fun ActivitasPertama(modifier: Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = stringResource(id= R.string.prodi),
+            text = stringResource(id = R.string.prodi),
             fontSize = 35.sp,
             fontWeight = FontWeight.Bold
         )
         Text(
-            text=stringResource(id= R.string.univ),
+            text = stringResource(id = R.string.univ),
             fontSize = 22.sp
         )
         Spacer(modifier = Modifier.height(height = 25.dp))
         Card(
             modifier = Modifier
-                .fillMaxWidth(fraction= 1f)
-                .padding(all= 12.dp),
+                .fillMaxWidth(fraction = 1f)
+                .padding(all = 12.dp),
             colors = CardDefaults.cardColors(
                 containerColor = Color.Black
             )
         ) {
             Row() {
-                val gambar = painterResource(id= R.drawable.logoumy)
+                val gambar = painterResource(id = R.drawable.logoumy)
                 Image(
                     painter = gambar,
                     contentDescription = null,
                     modifier = Modifier
-                        .size(size= 100.dp)
-                        .padding(all= 5.dp)
+                        .size(size = 100.dp)
+                        .padding(all = 5.dp)
                 )
-                Spacer(modifier = Modifier.width(width= 30.dp))
+                Spacer(modifier = Modifier.width(width = 30.dp))
                 Column() {
                     Text(
-                        text= stringResource(id= R.string.nama),
+                        text = stringResource(id = R.string.nama),
                         fontSize = 30.sp,
                         fontFamily = FontFamily.Cursive,
                         color = Color.White,
                         modifier = Modifier.padding(top = 15.dp)
                     )
                     Text(
-                        text= stringResource(id= R.string.alamat),
+                        text = stringResource(id = R.string.alamat),
                         fontSize = 20.sp,
                         color = Color.Yellow,
                         modifier = Modifier.padding(top = 10.dp)
@@ -76,11 +76,11 @@ fun ActivitasPertama(modifier: Modifier) {
                 text = stringResource(id = R.string.copy),
                 modifier = Modifier
                     .align(Alignment.BottomCenter)
-
+                    .padding(bottom = 50.dp)
             )
         }
-
-
+    }
+}
 
 
 
